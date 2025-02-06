9f821a6d932671ed0f0f339be48c28c77f6c4389
VulpineFriend87 <vulpinefriend87@gmail.com>
2025-02-06 15:36:13 +0100

    Create HelpSubCommand

---
+++ src/main/java/pro/vulpine/vCrates/command/subcommands/HelpSubCommand.java
@@ -0,0 +1,37 @@
+package pro.vulpine.vCrates.command.subcommands;
+
+import it.vulpinefriend87.easyutils.Colorize;
+import org.bukkit.command.CommandSender;
+import pro.vulpine.vCrates.command.VCratesCommand;
+import pro.vulpine.vCrates.instance.SubCommand;
+
+import java.util.Collections;
+import java.util.List;
+
+public class HelpSubCommand implements SubCommand {
+
+    private final VCratesCommand command;
+
+    public HelpSubCommand(VCratesCommand command) {
+        this.command = command;
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        List<String> help = command.getPlugin().getResponsesConfiguration().getStringList("help");
+        for (String line : help) {
+            sender.sendMessage(Colorize.color(line));
+        }
+
+    }
+
+    @Override
+    public List<String> executeTabComplete(CommandSender sender, String[] args) {
+        return Collections.emptyList();
+    }
+
+    public VCratesCommand getCommand() {
+        return command;
+    }
+}
